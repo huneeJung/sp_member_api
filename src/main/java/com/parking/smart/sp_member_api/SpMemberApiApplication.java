c870dce6380478bb52b727777ec237defcec7c2a
junghunee <cjswovkdnj12@naver.com>
2024-05-11 21:30:59 +0900

    init

---
+++ src/main/java/com/parking/smart/sp_member_api/SpMemberApiApplication.java
@@ -0,0 +1,13 @@
+package com.parking.smart.sp_member_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpMemberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpMemberApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parking/smart/sp_member_api/SpMemberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.smart.sp_member_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpMemberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
